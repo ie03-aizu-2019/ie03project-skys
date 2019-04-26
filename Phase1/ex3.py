6bceec0cfd5ca3714bfb7c5e442cf6091b1f435f
s1250131_Kimura <s1250131@u-aizu.ac.jp>
2019-04-26 15:21:29 +0900

    ルート探索に関する処理を完成(要テスト)

--- Phase1/ex3.py
+++ Phase1/ex3.py
@@ -36,6 +36,9 @@ class Manager:
 
         # root についてはあとで追記する
 
+    def output(self):
+
+
     def find_all_intersections(self):
         intersections = []
         segments = list(self.segments.values())
@@ -43,8 +46,10 @@ class Manager:
             for j in range(i, self.M):
                 tmp = ex1.find_intersection(segments[i], segments[j])
                 if tmp[0]:  # 交点あり
-                    # print(f"check A, {tmp[1].to_str()}")
-                    segments[i].set_contacted(tmp[1])
+                    # Segmentに接点情報を追加
+                    # segments[i].set_contacted(tmp[1])
+                    # segments[j].set_contacted(tmp[1])
+                    # tmp[1].set_contacted(segments)
                     if len(intersections) == 0:
                         intersections.append(tmp[1])
                     else:
@@ -72,33 +77,86 @@ class Manager:
                                 # 次のループへ
                                 continue
         self.intersections = intersections
-        list2dict(self.intersections)
+        list2dict(self.intersections, intersections=True)
 
     def search_root(self, start, fin):
-        roots = []
-        now = [start]
+        # start, finはポイントクラスオブジェクト
+        roots = self.searching(start, fin)
+        mins = []
+        mins.append(roots[0])
+        for i in range(1, len(roots)):
+            if mins[0][1] == roots[i][1]:
+                # 最短ルート追加
+                mins.append(roots[i][1])
+            elif mins[0][1] > roots[i][1]:
+                 # 最短ルート更新
+                 mins = []
+                 mins.append(roots[i])
 
-        for p in now:
-            for s in p.contacted:
+        if len(mins) == 0:
+            self.roots[start.index][fin.index] = [
+                ["NA", "NA"]
+            ]
+        else:
+            self.roots[start.index][fin.index] = [
+                [mins[0]]
+                # mins[0] = [経由点リスト, 距離]
+            ]
 
-    def contacted(self, token, fin):
+    def searching(self, start, fin, vias=[], roots=[]):
+        # start, finはポイントクラスオブジェクト
         # 再帰的に呼び出す
-        next = token.contacted
-        flag = False
-        for t in next:
-            if t is fin:
-                flag = True
+        # return 経由点
+
+        success = False
+        end = False
+        if start.isPoint():
+            vias.append(start)
+
+        if start is fin:
+            success = True
+
+        for via in vias:
+            if start is via:
+                end = True
                 break
 
+        if success:
+            dis = 0
+            for i in range(len(vias)-1):
+                dis += distance(vias[i], vias[i+1])
+            roots.append([vias, dis])
+
+        elif end:
+            pass
+
+        else:  # return条件を満たさなければ, 以下再帰へ.
+            tmp = []
+            for t in start.contacted:
+                result = self.search_root(t, fin, vias=vias, roots=roots)
+
+                # rootsのmerge
+                for r_info in result:
+                    flag = False
+                    for r_info2 in tmp:
+                        if r_info is r_info2:
+                            flag = True
+                    if flag:
+                        roots.append(r_info)
+
+        return roots
 
 
-
-
-def list2dict(l):
+def list2dict(l, intersections=False):
     length = len(l)
     d = {}
     for i in range(length):
-        d[i+1] = l[i]
-        # dの要素はPointまたはSegment
-        d[i+1].set_index(i+1)
+        if intersections:
+            d[f"C{i+1}"] = l[i]
+            # dの要素はPointまたはSegment
+            d[f"C{i+1}"].set_index(f"C{i+1}")
+        else:
+            d[f"{i+1}"] = l[i]
+            # dの要素はPointまたはSegment
+            d[f"{i+1}"].set_index(f"{i+1}")
     return d
