9b9ada9fc28f55860e1fc782c15b3f65c0b381fe
s1250133_koshikawa <s1250133@u-aizu.ac.jp>
2019-06-03 15:12:11 +0900

    hpge

--- source/Modules/manager.py
+++ source/Modules/manager.py
@@ -390,6 +390,31 @@ class Manager:
             if self.points[p].added and self.points[p].intersect:
                 print(f"{self.points[p].x:.6g} {self.points[p].y:.6g}")
 
+    def ex8(self):
+        for root in self.roots_index:
+            # root = ["開始", "終了"]
+            success_flag = True
+            try:
+                self.search_root(self.points[root[0]], self.points[root[1]])
+            except Exception:
+                # KeyError
+                success_flag = False
+            if success_flag:
+                res = self.roots[root[0]][root[1]]
+                # 順位(入力) - 1 = 順位に対応する経路の添字
+                res = res[int(root[2])-1]
+                # res = [経由点リスト, 距離]
+        #ここまでのプログラムはex4のプログラムを元に作成した
+        #ここから経由点リストに同じものがあればその経由点リストの両端の座標を出力させるプログラムを作れば良い
+                if res is None:  # 道無し
+                    print("NA")
+                if True:
+                    for point in res.points:
+                        print(point.index, end=" ")
+                    print()
+            else:
+                print("NA")
+
 
 def list2dict(l, intersections=False):
     if l is None:
