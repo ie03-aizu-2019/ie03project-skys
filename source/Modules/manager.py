cb2502a27099f1a6b45f579e11d6e4fb54bfb493
s1250131_Kimura <s1250131@u-aizu.ac.jp>
2019-06-16 16:07:50 +0900

    change

--- source/Modules/manager.py
+++ source/Modules/manager.py
@@ -332,7 +332,7 @@ class Manager:
             if success_flag:
                 res = self.roots[root[0]][root[1]]
                 # 順位(入力) - 1 = 順位に対応する経路の添字
-                res = res[int(root[2])-1]
+                res = res[0]
                 if res is None:  # 道無し
                     print("NA")
                 else:
